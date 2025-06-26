7cd838daad4116c5ff00a377cdeeadd2e2f33c45
Muhammad Arshman <itxarshman@gmail.com>
2025-06-26 16:10:59 +0500

    Update module-loader.js

--- modules/module-loader.js
+++ modules/module-loader.js
@@ -1,6 +1,6 @@
 const fs = require('fs-extra');
 const path = require('path');
-const logger = require('./logger'); // Fixed path
+const logger = require('..core//logger'); // Fixed path
 
 class ModuleLoader {
     constructor(bot) {
