c3253344f2f5e852d34def572651f7fdcb8582c7
Muhammad Arshman <itxarshman@gmail.com>
2025-06-29 16:28:46 +0500

    Update bot.js

--- core/bot.js
+++ core/bot.js
@@ -6,7 +6,7 @@ const path = require('path');
 const config = require('../config');
 const logger = require('./logger');
 const MessageHandler = require('./message-handler');
-const TelegramBridge = require('../bridge/telegram-bridge');
+const TelegramBridge = require('../watg/telegram-bridge');
 
 class AdvancedWhatsAppBot {
     constructor() {
