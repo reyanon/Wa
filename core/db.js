81724405b7f0606c0c212c8a0f36c1eff8d5399f
Muhammad Arshman <itxarshman@gmail.com>
2025-06-30 17:33:28 +0500

    Update db.js

--- core/db.js
+++ core/db.js
@@ -1,17 +1,26 @@
-const config = require('../config');
 const { MongoClient } = require('mongodb');
 
-const MONGO_URI = config.get('mongo.uri');
-const DB_NAME = config.get('mongo.dbName');
-const OPTIONS = config.get('mongo.options');
+const MONGO_URI = config.get('database.mongodb.uri');
+const DB_NAME = config.get('database.mongodb.dbName') || 'nexuswa';
+const OPTIONS = config.get('database.mongodb.options') || {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+};
 
 const client = new MongoClient(MONGO_URI, OPTIONS);
 
 async function connectDb() {
-    if (!client.topology?.isConnected()) {
+    if (!client.topology?.isConnected?.()) {
         await client.connect();
     }
     return client.db(DB_NAME);
 }
 
-module.exports = { connectDb };
+function useCollection(name) {
+    return async () => {
+        const db = await connectDb();
+        return db.collection(name);
+    };
+}
+
+module.exports = { connectDb, useCollection };
